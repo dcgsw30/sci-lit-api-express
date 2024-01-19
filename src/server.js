1c0c505d67fdfd93148cbcc575e9f0e73c66bf12
dcgsw30 <78886089+dcgsw30@users.noreply.github.com>
2024-01-19 01:15:50 -0800

    Transferred all code

---
+++ src/server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+// database array to store all documents
+let documents = [];
+
+// Json Parser Middleware
+app.use(bodyParser.json());
+
+// Display List
+app.get('/documents', (req, res, next) =>{
+  res.json(documents);
+});
+
+// Search for Literature
+app.get('/documents/search', (req, res, next) =>{
+  const providedLink = req.query.link;
+
+  if(!providedLink){
+    return res.status(400).send('No input found')
+  }
+
+  const wantedDocument = documents.find((document) => document.id === providedLink);
+
+  if (!wantedDocument){
+    return res.status(400).send('Document not found')
+  }
+
+  res.json(wantedDocument);
+
+});
+
+// Add New Literature
+app.post('/documents', (req, res, next) =>{
+  const newDocument = req.body;
+  documents.push(newDocument);
+  res.send(`The document ${newDocument.title} has been added!`)
+});
+
+// Delete New Literature
+app.delete('/documents/:link', (req, res, next) => {
+  const documentLink = req.params.link;
+  documents = documents.filter((document) => document.id !== documentLink);
+  res.send(`The document has been removed`)
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
